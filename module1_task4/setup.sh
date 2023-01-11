227c42027d6bf4ab35221dfb6804d52f8912f92c
edin93 <il_matador@hotmail.fr>
2023-01-11 04:07:48 -0800

    task 4, update script

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y hugo
-sudo apt-get install -y make
-rm -rf dist/
+apt-get update -y
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+source $HOME/.profile
+CGO_ENABLED=1 go install --tags extended github.com/gohugoio/hugo@latest
+apt-get install -y git
+apt-get install -y make
+make clean
 make build
